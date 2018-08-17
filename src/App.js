9614cbd7d005e0999229e19539b1c52161bf5603
Lucas Wang <lucas@Lucass-MacBook-Pro-2.local>
2018-08-17 11:24:41 +1000

    gallery image update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SectionCarousel from './gallery';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <div className="carousel-container">
+              <SectionCarousel/>
+          </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 it('renders without crashing', () => {
+    window.matchMedia = window.matchMedia || function() {
+        return {
+            matches : false,
+            addListener : function() {},
+            removeListener: function() {}
+        };
+    };
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
